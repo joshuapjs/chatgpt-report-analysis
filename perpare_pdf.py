4747e3a1d48733dcab2197590368eb93f5ac3326
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2023-08-02 23:22:48 +0200

    Adding text_cleaner

--- perpare_pdf.py
+++ perpare_pdf.py
@@ -1,5 +1,14 @@
 from pdfminer.high_level import extract_text
 
-path = "/Users/sping/Downloads/_10-Q-Q2-2023-As-Filed.pdf"
 
-print(extract_text(path))
+def textcleaner(pdf_file: str):
+    """
+    Function to select only lines that are long enough
+    From which I expect that they are pure text lines and therefore
+    relevant for and interpretable by ChatGPT
+    :param pdf_file: path to pdf file
+    """
+    text = extract_text(pdf_file).split('\n')  # split text into lines
+    paragraphs = [paragraph for paragraph in text if len(paragraph) > 60]  # select only lines that are long enough
+
+    return paragraphs

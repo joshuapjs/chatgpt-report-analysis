28d7cf33430c00f53194483bbe18e206002c02d1
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2023-08-03 08:03:33 +0200

    added get_response

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
+from prepare_pdf import textcleaner
 import openai
+import time
 import os
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# an example question about the 2022 Olympics
-query = input("How can I help you ?\n:")
+def get_response(file_path: str):
 
-response = openai.ChatCompletion.create(
-    messages=[
-        {'role': 'system', 'content': 'You answer questions about the 2022 Winter Olympics.'},
-        {'role': 'user', 'content': query},
-    ],
-    model=GPT_MODEL,
-    temperature=0,
-)
+    content = textcleaner(file_path)
+    every_response = []
 
-print(response['choices'][0]['message']['content'])
+    # first response
+    first_prompt = openai.Completion.create(
+        model="gpt-3.5-turbo",
+        prompt="You will know receive a lot of text. Please wait."
+               " I indicate when I am finished.",
+        max_tokens=7,
+        temperature=0,
+    )
+    every_response.append(first_prompt)
+
+    # text of the financial report
+    for prompt in content:
+        response = openai.Completion.create(
+              model="gpt-3.5-turbo",
+              prompt=prompt,
+              max_tokens=2000,
+              temperature=0,
+            )
+        every_response.append(response)
+
+    # last response
+    last_prompt = openai.Completion.create(
+        model="gpt-3.5-turbo",
+        prompt="Okay I am finished. please give me an overview about the current situation"
+               " and the most important facts.",
+        max_tokens=2000,
+        temperature=0,
+    )
+    every_response.append(last_prompt)
+
+    return every_response

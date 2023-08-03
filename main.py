a5c964f772c4d20476baeb3ab1f833ccf89e32be
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2023-08-03 09:19:21 +0200

    added first request

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from prepare_pdf import textcleaner
 import openai
 import time
+import pandas as pd
 import os
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -12,14 +13,14 @@ def get_response(file_path: str):
 
     # first response
     first_prompt = openai.Completion.create(
-        model="gpt-3.5-turbo",
-        prompt="You will know receive a lot of text. Please wait."
-               " I indicate when I am finished.",
+        model="text-davinci-003",
+        prompt="Hello? Can you hear me?",
         max_tokens=7,
         temperature=0,
     )
     every_response.append(first_prompt)
 
+    """
     # text of the financial report
     for prompt in content:
         response = openai.Completion.create(
@@ -29,6 +30,8 @@ def get_response(file_path: str):
               temperature=0,
             )
         every_response.append(response)
+        print(response)
+        time.sleep(4)
 
     # last response
     last_prompt = openai.Completion.create(
@@ -38,6 +41,10 @@ def get_response(file_path: str):
         max_tokens=2000,
         temperature=0,
     )
+    
     every_response.append(last_prompt)
+    """
 
     return every_response
+
+print(get_response('/Users/sping/Downloads/_10-Q-Q2-2023-As-Filed.pdf'))

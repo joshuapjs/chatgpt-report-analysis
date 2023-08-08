8e5b01d792ea0ed06308c041f58a2a61a40801ca
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2023-08-09 00:10:09 +0200

    updated default question

--- variables.py
+++ variables.py
@@ -3,7 +3,7 @@ variable =\
         "model": "gpt-3.5-turbo-16k",
         "request_size": 16384,
         "answer_size": 5000,
-        "question": "What is the current economic situation about the company based on the previous messages? "
-                    "Please give me no disclaimer - I know you can not give me reliable information. "
-                    "Just give me your opinion based on what I gave you."
+        "question": "What is the current economic situation of the company based on my previous messages? "
+                    "Give me no disclaimer - I know you can not give me reliable information. "
+                    "Give me your opinion only based on the previous messages."
      }
